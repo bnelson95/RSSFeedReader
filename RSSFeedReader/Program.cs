b2f8f0ece67f02fc54ddf00512e235278798bee1
Blake Nelson <bnelson95@gmail.com>
2021-03-15 20:34:58 -0400

    refactor Main to take in and validate a csv file path and a number of days as arguments

--- RSSFeedReader/Program.cs
+++ RSSFeedReader/Program.cs
@@ -1,51 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace RSSFeedReader
 {
+    class CompanyRSSFeed
+    {
+        public string Company { get; set; }
+        public string Url { get; set; }
+    }
+
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args">
+        /// First argument: File path to a headerless CSV (First column: Name, second column: URL)
+        /// Second argument: Number of days to check the feed against
+        /// </param>
         static void Main(string[] args)
         {
-            var companyFeeds = new Dictionary<string, List<string>>
+            if (args.Length != 2)
             {
-                { "ART19", new List<string> {
-                    "https://rss.art19.com/apology-line",
-                    "http://rss.art19.com/the-daily",
-                }},
-                { "Fireside", new List<string> {
-                    "https://feeds.fireside.fm/bibleinayear/rss",
-                }},
-                { "Megaphone", new List<string> {
-                    "https://feeds.megaphone.fm/ADL9840290619",
-                    "https://feeds.megaphone.fm/WWO3519750118",
-                }},
-                { "WNYC", new List<string> {
-                    "http://feeds.wnyc.org/experiment_podcast",
-                }},
-                { "Acast", new List<string> {
-                    "https://rss.acast.com/unraveled",
-                }},
-                { "Audioboom", new List<string> {
-                    "https://audioboom.com/channels/4997220.rss",
-                }},
-                { "NBC News", new List<string> {
-                    "https://podcastfeeds.nbcnews.com/dateline-nbc"
-                }},
-                { "Libsyn.com", new List<string> {
-                    "https://lincolnproject.libsyn.com/rss"
-                }},
-            };
-
-            var feedReader = new RSSFeedReader();
-            var inactiveCompanyFeeds = feedReader.FindInactiveFeeds(companyFeeds, 5).Result;
-
-            Console.WriteLine("Inactive Companies");
-            foreach (var company in inactiveCompanyFeeds)
-            {
-                Console.WriteLine($" - {company}");
+                Console.WriteLine("Please enter the correct arguments.");
+                ShowUsage();
+                return;
             }
+
+            Dictionary<string, List<string>> companyFeeds;
+            int days;
+
+            if (File.Exists(args[0]))
+            {
+                // Valid file path provided, parse feeds and group by company into dictionary
+                using (TextReader reader = new StreamReader(args[0]))
+                {
+                    var config = new CsvConfiguration(CultureInfo.CurrentCulture)
+                    {
+                        HasHeaderRecord = false,
+                    };
+                    var csvReader = new CsvReader(reader, config);
+                    companyFeeds = csvReader.GetRecords<CompanyRSSFeed>()
+                        .GroupBy(x => x.Company)
+                        .ToDictionary(k => k.Key, v => v.Select(x => x.Url).ToList());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid file path.");
+                ShowUsage();
+                return;
+            }
+
+            if (int.TryParse(args[1], out var numDays))
+            {
+                // Valid number of days provided
+                days = numDays;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number of days.");
+                ShowUsage();
+                return;
+            }
+
+            // All arguments are valid, check feeds and display
+            Console.WriteLine("Checking feeds...");
+            var feedReader = new RSSFeedReader();
+            var inactiveFeedNames = feedReader.FindInactiveFeeds(companyFeeds, days).Result;
+
+            if (inactiveFeedNames.Any())
+            {
+                Console.WriteLine($"Companies that have not updated their RSS feeds for {days} or more days:");
+                foreach (var name in inactiveFeedNames)
+                {
+                    Console.WriteLine($" - {name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No companies have left their RSS feeds inactive for {days} or more days.");
+            }
+        }
+
+        static void ShowUsage()
+        {
+            Console.WriteLine("Usage: RSSFeedReader [csv-file-path] [number-of-days]");
         }
     }
 }
